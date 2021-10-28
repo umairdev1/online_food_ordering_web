487a7804f4a2bf438d7247d5c0854ce8d4bdaebd
umairsaleem623 <umairsaleem623@gmail.com>
2021-10-28 00:57:02 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/online_food_ordering_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.online_food_ordering_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFe1eaf1);
+const kSecondaryColor = Color(0xFFfe5722);
